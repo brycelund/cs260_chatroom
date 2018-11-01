b567af7cd02c8526eb4abf58ac1653faf88b19a2
Bryce Lund <bryce.lund@gmail.com>
2018-11-01 09:55:39 -0600

    Remove user stuff

--- models/User.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-let userSchema = new mongoose.Schema({
-  username: String
-})
-
-
-module.exports = mongoose.model('User', userSchema);
